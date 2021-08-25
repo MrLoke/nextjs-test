8e27edbde9c12ec64b6dcbb83c4833ee9d5f16c7
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-08-25 22:23:20 +0200

    Add types for functions parameters

--- src/utils/getRecipientEmail.ts
+++ src/utils/getRecipientEmail.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 
 const getRecipientEmail = (
   users: string[],
-  userLoggedIn: firebase.User | null
+  userLoggedIn: firebase.User | null | undefined
 ) => {
   return users.filter((user) => user !== userLoggedIn?.email)[0]
 }
